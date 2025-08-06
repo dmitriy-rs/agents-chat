e1d7901d0e1447beb58fcd2525b20e50a53b829c
Dmitriy Popov <popov.dmitriiy@gmail.com>
2025-08-06 11:08:14 +0200

    Update BaseButton.vue

--- layers/base/app/components/BaseButton.vue
+++ layers/base/app/components/BaseButton.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 defineOptions({
-  inheritAttrs: true
+  inheritAttrs: true,
 })
 defineProps<{
   leadingIcon?: string
@@ -9,9 +9,16 @@ defineProps<{
 </script>
 
 <template>
-	<button v-bind="$attrs" class="mb-4 bg-[#c0c0c0] border-2 border-t-white border-l-white border-r-[#808080] border-b-[#808080] px-3 py-2 text-[11px] font-bold flex items-center gap-2 hover:bg-[#e0e0e0] active:border-t-[#808080] active:border-l-[#808080] active:border-r-white active:border-b-white h-8">
-	    <span v-if="leadingIcon" className="text-xl leading-none">{{ leadingIcon }}</span>
-		<slot />
-        <span v-if="trailingIcon" className="text-[16px] leading-none">{{ trailingIcon }}</span>
-	</button>
+  <button
+    v-bind="$attrs"
+    class="mb-4 bg-[#c0c0c0] border-2 border-t-white border-l-white border-r-[#808080] border-b-[#808080] px-3 py-2 text-[11px] font-bold flex items-center gap-2 hover:bg-[#e0e0e0] active:border-t-[#808080] active:border-l-[#808080] active:border-r-white active:border-b-white h-8"
+  >
+    <span v-if="leadingIcon" className="text-xl leading-none">{{
+      leadingIcon
+    }}</span>
+    <slot />
+    <span v-if="trailingIcon" className="text-[16px] leading-none">{{
+      trailingIcon
+    }}</span>
+  </button>
 </template>
